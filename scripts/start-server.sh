66ffac90c45d03a405c91fc1827eed34d894f4c0
maydeafmute <113387897+maydeafmute@users.noreply.github.com>
2025-01-14 00:14:25 +0100

    Update start-server.sh
    Use PORT env variable in vncserver.

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -3,7 +3,7 @@ export DISPLAY=:99
 export XAUTHORITY=/browser/.Xauthority
 
 echo "---Starting Services---"
-vncserver -geometry 1024x768 -depth 16 :99 -rfbport 5900 -noxstartup -securitytypes none 2>/dev/null
+vncserver -geometry 1024x768 -depth 16 :99 -rfbport ${PORT} -noxstartup -securitytypes none 2>/dev/null
 screen -d -m env HOME=/etc /usr/bin/fluxbox
 
 while true; do
